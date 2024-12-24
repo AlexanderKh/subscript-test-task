5bb3697444f264c70ad551b178b1207f50b4ed66
AlexanderKh <alexandr.khomchenko@gmail.com>
2024-12-24 17:31:38 +0200

    add login method

--- server/database/user-queries.js
+++ server/database/user-queries.js
@@ -1,7 +1,7 @@
 const knex = require("./connection.js");
 
 
-async function findByUsername(username) {
+async function findByUsername({ username }) {
     const results = await knex('users').where({ username: username });
     return results[0];
 }
@@ -11,8 +11,17 @@ async function createUser({ username, password_hash }) {
     return results[0];
 }
 
+async function saveSessionToken({ userId, sessionToken }) {
+    knex('users').where({ id: userId }).update(
+        {
+            session_token: sessionToken,
+        }
+    );
+}
+
 
 module.exports = {
     findByUsername,
     createUser,
+    saveSessionToken,
 }
--- server/routes/users-routes.js
+++ server/routes/users-routes.js
@@ -1,8 +1,9 @@
+const crypto = require('crypto')
 const express = require('express')
 const Joi = require('joi')
 const bcrypt = require('bcrypt')
 
-const {findByUsername, createUser} = require("../database/user-queries");
+const {findByUsername, createUser, saveSessionToken} = require("../database/user-queries");
 
 const router = express.Router()
 
@@ -10,9 +11,8 @@ const schema = Joi.object({
     username: Joi.string().min(3).max(100).required(),
     password: Joi.string().min(8).max(255).required(),
 })
-function validateUser(user) {
-    return schema.validate(user)
-}
+
+const COOKIE_TIMEOUT = 1000 * 60 * 60 * 24
 
 router.post('/register', async (req, res) => {
     const { error } = schema.validate(req.body)
@@ -37,5 +37,37 @@ router.post('/register', async (req, res) => {
     }
 });
 
+
+
+router.post('/login', async (req, res) => {
+    const { error } = schema.validate(req.body)
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
+
+    try {
+        let user = await findByUsername({ username: req.body.username })
+        if (!user) {
+            return res.status(400).send('Incorrect email or password.')
+        }
+        const correctPassword = await bcrypt.compare(req.body.password, user.password_hash)
+        if (!correctPassword) {
+            return res.status(400).json({ message: 'Incorrect email or password.' })
+        }
+
+        const sessionToken = crypto.randomBytes(64).toString('hex');
+        await saveSessionToken({ userId: user.id, sessionToken })
+
+        res.cookie(
+            "sessionToken", sessionToken, {
+                httpOnly: true,
+                sameSite: "strict",
+                maxAge: COOKIE_TIMEOUT
+            })
+        res.json({ message: 'Successfully logged in' })
+    } catch (err) {
+        return res.status(400).json({ message: err.message })
+    }
+});
 
 module.exports = router;
